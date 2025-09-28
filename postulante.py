1a3ef95be8536db0baa3a07941e31a0e71970f69
Abel <abeltorrescallapa@gmail.com>
2025-09-27 20:39:13 -0400

    postulante2

--- postulante.py
+++ postulante.py
@@ -6,3 +6,16 @@ class Postulante:
         self.modalidad = modalidad.strip()
         self.estado = "Registrado"
         self.nota = 0.0
+
+    def es_valido(self):
+        return (
+            self.ci.isdigit() and len(self.ci) >= 5 and
+            all([self.nombre, self.carrera, self.modalidad])
+        )
+
+    def resumen(self):
+        return f"{self.nombre} ({self.ci}) - {self.carrera} [{self.modalidad}]"
+
+
+
+
